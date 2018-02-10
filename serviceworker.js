534428bbb98a47266cb02cd42b0664045706d26d
Klaus Mueller <m@klml.de>
2018-02-10 14:58:41 +0100

    service worker js

---
+++ serviceworker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v21').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/Centre_mark.svg.png',
+        '/geoshare.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
